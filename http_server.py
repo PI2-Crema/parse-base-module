81fe5b1012fb034a2f038bf67590441a864b0f79
luisresende <luisfrvilela@gmail.com>
2017-10-27 21:02:46 -0200

    refactors in http_server.py

--- http_server.py
+++ http_server.py
@@ -7,18 +7,13 @@ import simplejson
 # HTTPRequestHandler class
 class RequestHandler(BaseHTTPRequestHandler):
 
-  # GET
   def do_GET(self):
-        # Send response status code
-        self.send_response(200)
 
-        # Send headers
+        self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        # Send message back to client
         message = "Hello Gabiras! Sou dev python agora"
-        # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
 
@@ -36,16 +31,11 @@ class RequestHandler(BaseHTTPRequestHandler):
         with open("data_server.txt", "w") as outfile:
             for key, value in data.items():
                 outfile.write(str(value) + ' ')
-                # simplejson.dump(" ".join(value), outfile)
-
 
         print("<----- Request End -----\n")
 
 def run():
   print('starting server...')
-
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
   server_address = ('', 8000)
   httpd = HTTPServer(server_address, RequestHandler)
   print('running server...')
