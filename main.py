c069b2244a0f2c099493dc29c17dd5a73944f5d3
luisresende <luisfrvilela@gmail.com>
2017-10-27 19:38:42 -0200

    change port to create http server

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
 import json
 import requests
+import os.path
+import time
 
 
 # HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):
 
   # GET
   def do_GET(self):
@@ -35,7 +35,6 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         print("\n----- Request Start ----->\n")
         print(request_path)
 
-
         if self.headers:
             request_headers = self.headers
             content_length = request_headers.get_all('content-length')
@@ -44,28 +43,49 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
             print(request_headers)
             print(self.rfile.read(length))
 
-
-
         print("<----- Request End -----\n")
-
 
 def run():
   print('starting server...')
 
   # Server settings
   # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 8000)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  server_address = ('', 8000)
+  httpd = HTTPServer(server_address, RequestHandler)
   print('running server...')
+  run_observer()
   httpd.serve_forever()
+
+def run_observer():
+    try:
+        while True:
+            time.sleep(1)
+            observer('node.txt')
+    except KeyboardInterrupt:
+        print("parei")
+
+def observer(filepath):
+    if os.path.isfile(filepath):
+        filename = filepath.split('.')
+        file = filename[0] + '.txt'
+        file_copy = filename[0] + '_copy.txt'
+        os.rename(
+            file,
+            file_copy
+            )
+
+        status_code = post_register_data(read_file(file_copy))
+        if status_code == 200:
+            delete_file(file_copy)
 
 # ID  HORA MINUTO NIVEL(0/100) BATERIA(0/100) CODE-ERROR  PH TEMPERATURA CODUTIVIDADE
 def post_register_data(post_fields):
     url = 'http://192.168.15.151:3000/feeders/register_data'
-    requests.post(url, json=post_fields)
+    post_request = requests.post(url, json=post_fields)
     print(post_fields)
+    return post_request.status_code
 
-def read_file():
+def read_file(file_name):
     keys = ["network_code",
             "hora",
             "minute",
@@ -77,7 +97,7 @@ def read_file():
             "conductivity"]
     json_data = {}
     array_data = []
-    with open("node.txt", "r") as f:
+    with open(file_name, "r") as f:
         data = f.readlines()
 
         for line in data:
@@ -94,4 +114,12 @@ def read_file():
     request_data = {'data':array_data}
     return request_data
 
-post_register_data(read_file())
+def delete_file(file_name):
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
